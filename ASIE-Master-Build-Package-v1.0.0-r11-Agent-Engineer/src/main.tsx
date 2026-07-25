d5a15e20bfd322bdb3075123199cd6361b0a3c25
alphasigma13579-lang <alphasigma13579@gmail.com>
2026-07-25 20:20:25 +0300

    feat(dib): mount Arabic DIB workspace route

--- ASIE-Master-Build-Package-v1.0.0-r11-Agent-Engineer/src/main.tsx
+++ ASIE-Master-Build-Package-v1.0.0-r11-Agent-Engineer/src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { AdminConsole } from "./AdminConsole";
+import { DIBWorkspace } from "./DIBWorkspace";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {window.location.hash === "#admin" ? <AdminConsole /> : <App />}
+    {window.location.hash === "#admin" ? <AdminConsole /> : window.location.hash === "#dib" ? <DIBWorkspace /> : <App />}
   </React.StrictMode>
 );
