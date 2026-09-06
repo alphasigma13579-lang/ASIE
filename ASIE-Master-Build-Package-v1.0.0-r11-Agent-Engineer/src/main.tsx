48e3c50f09c0a2639cf5455be12467cb34d8980e
alphasigma13579-lang <284006821+alphasigma13579-lang@users.noreply.github.com>
2026-09-06 23:01:21 +0300

    feat(i18n): mount customer language provider

--- ASIE-Master-Build-Package-v1.0.0-r11-Agent-Engineer/src/main.tsx
+++ ASIE-Master-Build-Package-v1.0.0-r11-Agent-Engineer/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { AdminConsole } from "./AdminConsole";
 import { ASIECompleteSurfaceMount } from "./ASIECompleteSurfaceMount";
+import { CustomerLanguageProvider } from "./customerLanguage";
 import { DIBControlledFinanceWiring } from "./DIBControlledFinanceWiring";
 import { DIBE2EScenario } from "./DIBE2EScenario";
 import { DIBIntakeItemGovernance } from "./DIBIntakeItemGovernance";
@@ -37,8 +38,10 @@ const showDIBCardDirectAction = !currentHash.startsWith("#admin") && !currentHas
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {routedApp}
-    <ASIECompleteSurfaceMount />
-    {showDIBCardDirectAction ? <DIBProjectCardDirectActionMount /> : null}
+    <CustomerLanguageProvider>
+      {routedApp}
+      <ASIECompleteSurfaceMount />
+      {showDIBCardDirectAction ? <DIBProjectCardDirectActionMount /> : null}
+    </CustomerLanguageProvider>
   </React.StrictMode>
 );
